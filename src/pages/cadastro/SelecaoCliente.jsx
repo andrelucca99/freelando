69b54c9cb12f6e54fc88de8dc23418efa652420b
André Lucas <andrelucca99@gmail.com>
2024-03-28 09:46:51 -0300

    feat: add links de navegação

--- src/pages/cadastro/SelecaoCliente.jsx
+++ src/pages/cadastro/SelecaoCliente.jsx
@@ -1,5 +1,6 @@
 import { Col, Row } from "react-grid-system";
 import { Tipografia } from "../../components/Tipografia";
+import { Link as RouterLink } from "react-router-dom"
 
 import cliente from './assets/cliente.png';
 import freela from './assets/freela.png';
@@ -7,27 +8,29 @@ import { Link } from "../../components/Link";
 
 const SelecaoCliente = () => {
   return (
-    <>
+    <div style={{ textAlign: 'center' }}>
       <Tipografia variante="h1" componente="h1">Crie seu cadastro</Tipografia>
       <Tipografia variante="h3" componente="h2">Como podemos te ajudar?</Tipografia>
 
       <Row>
         <Col md={6} sm={12}>
-          <img src={cliente} alt="Foto de uma pessoa cliente" />
-          <Tipografia variante="body" componente="body">Sou cliente e preciso de um freela!</Tipografia>
+          <RouterLink to="/interesses">
+            <img src={cliente} alt="Foto de uma pessoa cliente" />
+            <Tipografia variante="body" componente="body">Sou cliente e preciso de um freela!</Tipografia>
+          </RouterLink>
         </Col>
         <Col md={6} sm={12}>
           <img src={freela} alt="Foto de uma pessoa freelancer" />
           <Tipografia variante="body" componente="body">Sou um freela e preciso de clientes!</Tipografia>
         </Col>
       </Row>
-      <div style={{ textAlign: 'center' }}>
+      <div>
         <Tipografia variante="body2" componente="body2">Já tem conta?</Tipografia>
         <p>
           <Link variante="secundaria">Faça login!</Link>
         </p>
       </div>
-    </>
+    </div>
   )
 }
 
